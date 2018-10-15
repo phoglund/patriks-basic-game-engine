d77255176e42fab99d0276994a69c55ca2450a2d
Patrik Höglund <patrik.hoeglund@gmail.com>
2018-10-15 21:18:35 +0200

    Slowly draws a line

--- painter.py
+++ painter.py
@@ -5,16 +5,25 @@ class Painter(object):
 
   def demo(self):
     pygame.init()
+    pygame.display.set_caption('Mario Clone')
     screen = pygame.display.set_mode((640, 480))
 
-    px = pygame.PixelArray(screen)
-    for x in range(128):
-      px[x, x] = pygame.Color(255, 255, 255)
-
-    pygame.display.flip()
+    clock = pygame.time.Clock()
 
     running = True
+    px = pygame.PixelArray(screen)
+    x = 0.0
+    y = 0
     while running:
+      dt = clock.tick(60)
+      for a in range(128):
+        px[int(x), y] = pygame.Color(255, 255, 255)
+        x += 1
+        if x >= 640:
+          x = 0.0
+          y += 1
+
+      pygame.display.flip()
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           running = False
