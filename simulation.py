688f2ecdb87fb4b65f3478a441fe1a4af98704f7
Patrik Höglund <patrik.hoeglund@gmail.com>
2018-10-16 20:58:17 +0200

    Can move left or right

--- simulation.py
+++ simulation.py
@@ -1,29 +1,7 @@
 import pygame
 import random
 
-
-class Obstacle(object):
-
-  def __init__(self, rect, color):
-    self._rect = rect
-    self._color = color
-
-  def draw_to(self, screen, viewpoint_pos_x):
-    translated = self._rect.move(-viewpoint_pos_x, 0)
-    pygame.draw.rect(screen, self._color, translated)
-
-
-def random_obstacle(bounds):
-  x_end, y_end = bounds
-  size = 40
-  x = random.randint(0, 5 * x_end)
-  y = random.randint(0, y_end - size)
-
-  rect = pygame.Rect(x, y, size, size)
-  grayscale = random.randint(0, 255)
-  color = pygame.Color(grayscale, grayscale, grayscale)
-
-  return Obstacle(rect, color)
+import obstacles
 
 
 class Simulation(object):
@@ -33,12 +11,29 @@ class Simulation(object):
     self._size = size
     self._viewpoint_pos_x = 0.0
     self._viewpoint_x_speed = 5.0
-    self._obstacles = [random_obstacle(size) for _ in xrange(20)]
+    self._obstacles = [obstacles.random_obstacle(size)
+                       for _ in xrange(20)]
 
-  def advance(self, fraction):
-    self._viewpoint_pos_x += self._viewpoint_x_speed * fraction
+  def advance(self, time_fraction):
+    speed = self._player_speed()
+
+    self._viewpoint_pos_x += speed * time_fraction
     self._draw_ground()
     self._draw_obstacles(self._viewpoint_pos_x)
+
+  def _player_speed(self):
+    pressed = pygame.key.get_pressed()
+    moving_left = pressed[pygame.K_LEFT]
+    moving_right = pressed[pygame.K_RIGHT]
+    if moving_left and moving_right:
+      # Don't move in this case.
+      return 0
+    elif moving_left:
+      return -self._viewpoint_x_speed
+    elif moving_right:
+      return self._viewpoint_x_speed
+    else:
+      return 0
 
   def _draw_ground(self):
     ground_color = pygame.Color(255, 128, 255)
